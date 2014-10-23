2646ef60e52ba85612a26d04eba75a655697c9ce
hungwpv <hungwpv@outlook.com>
2014-10-23 11:16:03 +0700

    WPV FIzz
    Fizz

---
+++ WPV Fizz/FizzSlaveOfPoseidon/Program.cs
@@ -0,0 +1,13 @@
+﻿using LeagueSharp;
+using LeagueSharp.Common;
+namespace WPVFizz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += WPVFizz.OnGameLoad;
+        }
+       
+    }
+}
